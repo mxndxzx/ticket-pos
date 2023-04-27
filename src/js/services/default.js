9d7f1fb09ff28a3a469c7a83c1e0d727ec811c53
bmariani <bmariani@rpba.gob.ar>
2023-04-27 11:15:00 -0300

    Revert "Changed printer method"
    This reverts commit 14963482727a7832def5fb11cf8e0901484ede00.

--- src/js/services/default.js
+++ src/js/services/default.js
@@ -11,15 +11,14 @@ console.log(`Serialport:: ${serialport}`)
 
 class Printer {
     async prnt(data) {
-        const printer = new ThermalPrinter({
+        let printer = new ThermalPrinter({
             type: PrinterTypes.EPSON,                 // Printer type: 'star' or 'epson'
-            // interface: process.argv[2],               // Printer interface
-            interface: '//./COM1',               // Printer interface
+            interface: process.argv[2],               // Printer interface
             characterSet: CharacterSet.PC852_LATIN2,  // Printer character set - default: SLOVENIA
             removeSpecialCharacters: false,           // Removes special characters - default: false
             lineCharacter: "-",                       // Set character for lines - default: "-"
             breakLine: BreakLine.WORD,                // Break line after WORD or CHARACTERS. Disabled with NONE - default: WORD
-            width: 40,                                // Number of characters per line
+            width: 40                                 // Number of characters per line
         });
 
         let isConnected = await printer.isPrinterConnected();
@@ -33,8 +32,7 @@ class Printer {
         printer.newLine();
 
         try {
-            const call = await printer.execute();
-            console.log(`Default call:: ${call}`);
+            await printer.execute();
             return data
         } catch (err) {
             console.log(`Default error:: ${err}`)
