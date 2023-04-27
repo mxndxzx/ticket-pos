37962c90d0acabaec1a60366a8b772b03f5be6f1
bmariani <bmariani@rpba.gob.ar>
2023-04-27 14:47:39 -0300

    COM1 config v2

--- src/js/services/default.js
+++ src/js/services/default.js
@@ -4,21 +4,17 @@ const { SerialPort } = require('serialport');
 const city = 'La Plata';
 let today = new Date().toLocaleDateString();
 
-const serialport = new SerialPort({ path: 'COM1', baudRate: 9600 });
-
 class Printer {
     async prnt(data) {
         let printer = new ThermalPrinter({
             type: PrinterTypes.EPSON,                 // Printer type: 'star' or 'epson'
-            interface: '//./COM1',               // Printer interface
+            interface: 'COM1',               // Printer interface
             characterSet: CharacterSet.PC852_LATIN2,  // Printer character set - default: SLOVENIA
             removeSpecialCharacters: false,           // Removes special characters - default: false
             lineCharacter: "-",                       // Set character for lines - default: "-"
             breakLine: BreakLine.WORD,                // Break line after WORD or CHARACTERS. Disabled with NONE - default: WORD
             width: 40                                 // Number of characters per line
         });
-        
-        serialport.write('Lorem ipsum sit dolor amet');
 
         let isConnected = await printer.isPrinterConnected();
         console.log(`Printer connected? ${isConnected}`);
